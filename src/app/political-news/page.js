eb34b3820897c40068e0be65c2037bc80251d924
Fred <huertaserick@yahoo.ie>
2025-02-24 19:58:41 +0000

    Added loading

---
+++ src/app/political-news/page.js
@@ -0,0 +1,14 @@
+// const getData = async () => {
+//   await new Promise((resolve) => setTimeout(resolve, 1000000)); // Simulate 3s delay
+//   return { message: "Data loaded!" };
+// };
+
+export default async function Page() {
+  //   const data = await getData();
+
+  return (
+    <div className="container mx-auto p-10">
+      <h1 className="text-3xl font-bold">{data.message}</h1>
+    </div>
+  );
+}
--- src/app/sports-news/page.jsx
+++ src/app/sports-news/page.jsx
@@ -4,7 +4,6 @@ const getNews = async () => {
   const { data } = await axios.get(
     `https://content.guardianapis.com/search?section=sport&api-key=${process.env.NEXT_PUBLIC_GUARDIAN_API_KEY}`
   );
-  console.log(data);
   return data.response.results;
 };
 
@@ -12,19 +11,27 @@ const SportsNews = async () => {
   const articles = await getNews();
 
   return (
-    <section>
-      <h1 className="text-2xl font-bold">Latest News</h1>
-      {articles.map((article) => (
-        <div key={article.id}>
-          <h2 className="text-xl font-bold">{article.webTitle}</h2>
-          <p>
-            Source:
-            <a href={article.webUrl} className="text-blue-500">
-              The Guardian
-            </a>
-          </p>
-        </div>
-      ))}
+    <section className="container m-auto pt-20">
+      <h1 className="text-4xl font-bold p-5">
+        Latest Sports News &copy;The Guardian
+      </h1>
+      <div className="grid grid-cols-2 gap-3">
+        {articles.map((article) => (
+          <div key={article.id} className="border rounded border-zinc-700 p-3">
+            <h2 className="text-xl font-bold pb-3">{article.webTitle}</h2>
+            <p className="">
+              Source:
+              <a
+                href={article.webUrl}
+                target="_blank"
+                className="text-blue-600 cursor-pointer font-semibold pl-2"
+              >
+                The Guardian
+              </a>
+            </p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
